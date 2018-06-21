a22760198d6544676cb8cd80be074262f7b346b3
patilvr <patil.v.r94@gmail.com>
2018-06-21 00:56:07 -0700

    first commit

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+def post_new(request):
+
+if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+ return render(request, 'blog/form.html', {})
+
+
+# Create your views here.
